ef95372efd5387c10034cf427e547506a6e40a36
raw1244ff <rushikesh.khfug22@sinhgad.edu>
2025-09-19 16:39:54 +0530

    Refactor app.js to use wrapAsync for error handling and add ExpressError class; update index.ejs for price formatting; create .gitignore file

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const methodoverride = require("method-override");
 const ejsmate=require("ejs-mate");
+const wrapasync = require("./utils/wrapAsync.js");
+const expressError= require("./utils/expressError.js");
+const ExpressError = require("./utils/expressError.js");
+
+
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -26,10 +31,10 @@ async function main(){
 };
 
 //index route
-app.get("/listings",async (req,res) =>{
+app.get("/listings",wrapasync(async (req,res) =>{
     const allListing = await Listing.find({});
     res.render("./listing/index.ejs",{allListing});
-});
+}));
 
 //new route
 app.get("/listing/new",(req,res)=>{
@@ -37,42 +42,51 @@ app.get("/listing/new",(req,res)=>{
 });
 
 //show route
-app.get("/listings/:id",async (req,res)=> {
+app.get("/listings/:id",wrapasync(async (req,res)=> {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listing/show.ejs",{listing});
-});
+}));
 
 //creat route
-app.post("/listings",async (req,res) => {
+app.post("/listings",wrapasync(async (req,res) => {
     // let{title, description, image, price, country ,location} =req.body;
     let newlisting = new Listing(req.body.listing);
     await newlisting.save();
     res.redirect("/listings");
-});
+})
+);
 
 //edit route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapasync(async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listing/edit.ejs",{listing});
-});
+}));
 
 //update route
-app.put("/listings/:id",async (req,res) =>{
+app.put("/listings/:id",wrapasync(async (req,res) =>{
     let{id} = req.params;
    await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+}));
 
 //delete route
-app.delete("/listings/:id",async (req,res)=>{
-   let {id} = req.params;
+app.delete("/listings/:id",wrapasync(async (req,res)=>{
+    let {id} = req.params;
    let deletedlisting = await Listing.findByIdAndDelete(id);
    console.log(deletedlisting);
-   res.redirect("/listings");
-});
+   res.redirect("/listings")
+}));
 
+app.all("*",(err,req,res,next)=>{
+    next(new ExpressError(404,"page not found!"));
+});
+ 
+app.use((err,req,res,next) =>{
+    let{statuscode=500,message="something went wrong!"} = err;
+    res.status(statuscode).send(message);   
+});
 
 app.listen(3000,(req,res) =>{
     console.log("listening to port 3000");
---
+++ utils/wrapAsync.js
@@ -0,0 +1,5 @@
+modute.exports = (fn) => {
+    return (req, res, next) => {
+        fn(req, res, next).catch(next)
+ };
+};
